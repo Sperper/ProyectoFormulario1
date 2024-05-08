df3556f58ab46aae53701b2fa47bbd03f59a7d2a
sperper2907 <sperper2907@g.educaand.es>
2024-05-08 14:50:51 +0200

    Prog

--- src/main/java/com/es/proyectoformulario/ui/panels/PanelModificar.java
+++ src/main/java/com/es/proyectoformulario/ui/panels/PanelModificar.java
@@ -4,6 +4,7 @@ import com.es.proyectoformulario.services.impl.ServiceUser;
 import com.es.proyectoformulario.ui.frames.FrameLogin;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class PanelModificar extends JPanel {
@@ -11,8 +12,9 @@ public class PanelModificar extends JPanel {
     private JTextField textUser;
     private JTextField textRepPass;
     private JTextField textPass;
-    private JButton bEnviar;
+    private JButton bModificar;
     private JButton bConsultar;
+    private JButton bVolver;
     private JTextField textIdUser;
     private JTextField textIdUser2;
     private ServiceUser serviceUser = new ServiceUser();
@@ -91,6 +93,31 @@ public class PanelModificar extends JPanel {
         textRepPass.setSize(new Dimension(120,32));
         this.add(textRepPass);
 
+        JLabel labelIsAdmin = new JLabel("is admin");
+        labelIsAdmin.setLocation(new Point(193, 400));
+        labelIsAdmin.setSize(new Dimension(120,32));
+        labelIsAdmin.setFont(new Font("Consolas", Font.BOLD, 15));
+        this.add(labelIsAdmin);
+
+        JComboBox<String> isAdmin = new JComboBox<>();
+        isAdmin.setLocation(new Point(275,400));
+        isAdmin.setSize(new Dimension(120, 32));
+        isAdmin.setBackground(new Color(255,255,255));
+        isAdmin.addItem(null);
+        isAdmin.addItem("No");
+        isAdmin.addItem("Si");
+        this.add(isAdmin);
+
+        bModificar = new JButton("MODIFICAR");
+        bModificar.setLocation(new Point(225,450));
+        bModificar.setSize(new Dimension(120,32));
+        bModificar.setBackground(new Color(105, 175, 216));
+        bModificar.setBorder(new LineBorder(new Color(135, 206, 250),2));
+        this.add(bModificar);
+
+
     }
+
+
 
 }
